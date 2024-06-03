6af1f92f8273666d5090fedab4421ad1153d612a
HenrietteDaughtyOloo <henriettedoloo@gmail.com>
2024-06-03 12:50:50 +0300

    Started Integration with MPesa

--- lib/activities/payment_screen.dart
+++ lib/activities/payment_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shop/activities/complete_screen.dart';
+import 'package:intl/intl.dart';
+
+
 
 class PaymentScreen extends StatefulWidget {
   final double totalAmount;
@@ -13,6 +19,72 @@ class PaymentScreen extends StatefulWidget {
 class _PaymentScreenState extends State<PaymentScreen> {
   String? _selectedPaymentMode;
   TextEditingController mobileNumberController = TextEditingController();
+
+  Future<void> initiatePayment(String phoneNumber) async{
+    final consumerKey = dotenv.env['DARAJA_CONSUMER_KEY'];
+    final consumerSecret = dotenv.env['DARAJA_CONSUMER_SECRET'];
+    final auth = base64Encode(utf8.encode('$consumerKey:$consumerSecret'));
+
+    final tokenResponse = await http.get(
+      Uri.parse('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'),
+      headers: {'Authorization': 'Basic $auth'},
+    );
+    final accessToken = jsonDecode(tokenResponse.body)['access_token'];
+
+    // Generate the password for the request
+    // String timestamp = DateFormat('yyyyMMddHHmmss').format(DateTime.now());
+    // String password = base64Encode(utf8.encode('174379' + 'your_lipa_na_mpesa_online_passkey' + timestamp));
+
+
+String generatePassword(String shortCode, String passKey, String timestamp) {
+  final password = base64Encode(utf8.encode('$shortCode$passKey$timestamp'));
+  return password;
+}
+
+String getTimestamp() {
+  final now = DateTime.now();
+  final formatter = DateFormat('yyyyMMddHHmmss');
+  return formatter.format(now);
+}
+
+final String timestamp = getTimestamp();
+final String password = generatePassword("174379", "your_pass_key", timestamp);
+
+
+
+    final paymentResponse = await http.post(
+      Uri.parse('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'),
+      headers: {
+        'Authorization': 'Bearer $accessToken',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        "BusinessShortCode": "174379",
+        "Password": password,
+        "Timestamp": timestamp,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": widget.totalAmount,
+        "PartyA": phoneNumber,
+        "PartyB": "174379",
+        "PhoneNumber": phoneNumber,
+        "CallBackURL": "your_callback_url",
+        "AccountReference": "account_reference",
+        "TransactionDesc": "Payment description"
+      }),
+    );
+
+    if (paymentResponse.statusCode == 200) {
+      print('Payment initiated successfully');
+      _showPaymentPrompt(context, phoneNumber);
+    } else {
+      print('Failed to initiate payment: ${paymentResponse.body}');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to initiate payment. Please try again.')),
+      );
+    }
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -46,12 +118,11 @@ class _PaymentScreenState extends State<PaymentScreen> {
                 setState(() {
                   _selectedPaymentMode = newValue;
                   if (_selectedPaymentMode == 'Mobile Money') {
-                    // Show pop-up for mobile number input
                     _showMobileNumberInputPopup(context);
                   }
                 });
               },
-              items: <String>['Cash', 'Mobile Money', 'Debit Card']
+              items: <String>['Cash', 'Mobile Money']
                   .map<DropdownMenuItem<String>>((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
@@ -238,7 +309,9 @@ class _PaymentScreenState extends State<PaymentScreen> {
     );
   }
 
-  void _showPaymentPrompt(BuildContext context, String mobileNumber) {
+void _showPaymentPrompt(BuildContext context, String mobileNumber) async {
+  try {
+    await initiatePayment(mobileNumber);
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
@@ -259,21 +332,97 @@ class _PaymentScreenState extends State<PaymentScreen> {
         actions: [
           TextButton(
             onPressed: () {
-              Navigator.pop(context); // Close the dialog
+              Navigator.pop(context); 
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => ShopAgain(), 
+                  builder: (context) => ShopAgain(),
                 ),
-              );
+              ).then((_) {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text(
+                      'Payment Successful',
+                      style: TextStyle(
+                        color: Color(0xFF03787C),
+                        fontSize: 17,
+                      ),
+                    ),
+                    content: Text(
+                      'Your payment of \KES ${widget.totalAmount.toStringAsFixed(2)} was successful.',
+                      style: TextStyle(
+                        color: Color(0xFF03787C),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ShopAgain(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          'OK',
+                          style: TextStyle(
+                            color: Color(0xFF03787C),
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              });
             },
             child: Text('OK'),
           ),
         ],
-        
+      ),
+    );
+  } catch (e) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(
+          'Payment Failed',
+          style: TextStyle(
+            color: Color(0xFF03787C),
+            fontSize: 17,
+          ),
+        ),
+        content: Text(
+          'Your payment of \KES ${widget.totalAmount.toStringAsFixed(2)} failed. Please try again later.',
+          style: TextStyle(
+            color: Color(0xFF03787C),
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              'OK',
+              style: TextStyle(
+                color: Color(0xFF03787C),
+                fontSize: 17,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
 
   @override
   void dispose() {
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/model/cart_model.dart';
 import 'activities/splash_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'activities/payment_screen.dart';
 
-void main() {
+
+Future<void> main() async {
+  String envPath = '${Directory.current.path}/.env';
+  print('Loading .env file from: $envPath');
+  
+  try {
+    await dotenv.load(fileName: envPath);
+    print("ENV Loaded: ${dotenv.env}");
+  } catch (e) {
+    print("Error loading .env file: $e");
+  }
   runApp(const MyApp());
 }
 
